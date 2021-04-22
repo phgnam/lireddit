d9fea369d95d7857f8605312bfbfcc8fed9d8f63
Phuong Nam <nam.nguyenphuong@savvycomsoftware.com>
2021-04-22 18:19:32 +0700

    Update

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -12,42 +12,116 @@ class UsernamePasswordInput {
 }
 
 @ObjectType()
-class UserResponse {
+class FieldError {
   @Field()
-  errors: Error[]
+  field: string
 
   @Field()
-  user: User
+  message: string
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true })
+  user?: User
 }
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Query(() => User, { nullable: true })
+  async me(
+    @Ctx() { req, em }: MyContext
+  ) {
+    console.log("session: ", req.session)
+    // You are not logged in
+    if (!req.session.userId) {
+      return null
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
-  ) {
-    const hashedPassword = argon2.hash(options.password)
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'length must be greater than 2'
+        }]
+      }
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [{
+          field: 'password',
+          message: 'length must be greater than 3'
+        }]
+      }
+    }
+
+    const userInDb = await em.findOne(User, { username: options.username.toLowerCase() })
+    if (userInDb) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'username already taken'
+        }]
+      }
+    }
+    const hashedPassword = await argon2.hash(options.password)
     const user = em.create(User, {
       username: options.username.toLowerCase(),
       password: hashedPassword
     })
     await em.persistAndFlush(user)
-    return user
+
+    // store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id
+
+    return { user }
   }
 
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async login(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
-  ) {
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username.toLowerCase() })
     if (!user) {
       return {
-        errors: [{}]
+        errors: [
+          {
+            field: 'username',
+            message: "that username doesn't exist",
+          },
+        ],
       }
     }
-    const hashedPassword = await argon2.hash(options.password)
-    return user
+    const valid = await argon2.verify(user.password, options.password)
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: "incorrect password",
+          },
+        ]
+      }
+    }
+
+    req.session.userId = user.id
+
+    return { user }
   }
 }
